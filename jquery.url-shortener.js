c83415cc20c3f6b8da5fd1fa75fc3d036615ea00
spencercarnage <spencercarnage@gmail.com>
2013-05-15 18:07:52 -0700

    finished early first draft with tests!

--- jquery.url-shortener.js
+++ jquery.url-shortener.js
@@ -1,43 +1,214 @@
 /**
  *
- * Converts a URL form input into a bit.ly link.
+ * Converts a URL form input into a shortened link.
  *
  */
 
-function URLShortener() {
-  this._defaults = {
-    urlRE: /(https?:\/\/)?((\w+:{0,1}\w*@)?(\S+)\.[a-zA-Z]{2,})(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/,
-    shortenerRE: /bit\.ly/, // Regex for URL shortening service
-    shortenAPI: '', // AJAX endpoint for URL shortening service
-    onSuccess: function () {}
-  };
+;(function ($) {
+  function URLShortener() {
+    this._defaults = {
+      urlRE: /(https?:\/\/)?((\w+:{0,1}\w*@)?(\S+)\.[a-zA-Z]{2,})(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/,
+      shortenerRE: /bit\.ly/, // Regex for URL shortening service.
+      api: undefined, // AJAX endpoint for URL shortening service.
+      onSuccess: null
+    };
+  }
 
-  this._attachPlugin = function attachPlugin() {
+  $.extend(URLShortener.prototype, {
+    events: 'blur.url-shortener input.url-shortener propertychange.url-shortener',
 
-  };
+    shortenerClass: 'url-shortener',
 
-  this._validURL = function validURL(url) {
-    if (!bitlyFlag && urlRE.test(url) && !bitlyRE.test(url)) {
-      return true;
-    } else {
-      return false;
+    propertyName: 'urlshortener',
+
+    _shortened: false, // Flag for determining whether or not URL was shortened.
+
+    _validURL: function validURL(url) {
+      if (!this._shortened && this._defaults.urlRE.test(url) && !this._defaults.shortenerRE.test(url)) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+
+    /*
+     * Override the default settings for all URL shortener instances.
+     * @param options (object) the new settings to use as defaults
+     * @return (URLShortener) this object
+     */
+    setDefaults: function (options) {
+      $.extend(this._defaults, options || {});
+      return this;
+    },
+
+    /*
+     * Attach the URL shortening functionality to a text input.
+     * @param target (element) the input to affect
+     * @param options (object) the custom options for this
+     */
+    _attachPlugin: function (target, options) {
+      target = $(target);
+
+      if (target.hasClass(this.shortenerClass)) {
+        return;
+      }
+
+      var instance = {
+        options: $.extend({}, this._defaults)
+      };
+
+      target.addClass(this.shortenerClass)
+        .data(this.propertyName, instance)
+        .bind(this.events, function (event) {
+          
+        });
+      
+      this._optionPlugin(target, options);
+    },
+
+    /*
+     * Retrieve or reconfigure the settings for a control.
+     * @param target (element) the control to affect
+     * @param options (object) the new options for this instance or
+     *                (string) an individual property name
+     *  @param value (any) the individual property value (omit if options is an
+     *                ojbect or to retrieve the value of a setting)
+     *  @return (any) if retrieving a value
+     */
+    _optionPlugin: function (target, options, value) {
+      target = $(target);
+
+      var instance = target.data(this.propertyName);
+      var name;
+
+      if (!options || 
+          (typeof options === 'string' && typeof value === 'undefined')) {
+        
+        // Get option.
+
+        name = options;
+        options = (instance || {}).options;
+        return (options && name ? options[name] : options);
+      }
+
+      if (!target.hasClass(this.shortenerClass)) {
+        return;
+      }
+
+      options = options || {};
+
+      if (typeof options === 'string') {
+        name = options;
+        options = {};
+        options[name] = value;
+      }
+
+      $.extend(instance.options, options);
+
+      if (typeof instance.options.api === 'undefined') {
+        this._destroyPlugin(target[0]);
+        console.error('No API endpoint was defined in the options.');
+        throw 'No API endpoint was defined in the options.';
+      }
+    },
+
+    shortenURL: function shortenURL (target, url) {
+      target = $(target);
+
+      var scope = this;
+      var instance = target.data(this.propertyName);
+
+      $.post(instance.options.api, {url: url}, function (data, textStatus, jqXHR) {
+        if (typeof instance.options.onSuccess === "function") {
+          instance.options.onSuccess.apply(scope, arguments);
+        }
+      });
+    },
+
+    /*
+     * Enable the URL shortener.
+     * @param target (element) the control to affect
+     */
+    _enablePlugin: function (target) {
+      target = $(target);
+
+      if (!target.hasClass(this.shortenerClass)) {
+        return;
+      }
+
+      target.prop('disabled', false).removeClass('url-shortener-disabled');
+      var instance = target.data(this.propertyName);
+    },
+
+    /*
+     * Disable the control.
+     * @param target (element) the control to affect
+     */
+    _disablePlugin: function (target) {
+      target = $(target);
+
+      if (!target.hasClass(this.shortenerClass)) {
+        return;
+      }
+
+      target.prop('disabled', true).addClass('url-shortener-disabled');
+      var instance = target.data(this.propertyName);
+    },
+
+    _destroyPlugin: function (target) {
+      target = $(target);
+      if (!target.hasClass(this.shortenerClass)) {
+        return;
+      }
+
+      var instance = target.data(this.propertyName);
+
+      target.removeClass(this.shortenerClass)
+            .removeData(this.propertyName)
+            .unbind('.url-shortener');
     }
-  };
+  });
 
-  this.shortenURL = function shortenURL (url) {
-    $.post(this.shortenerAPI, {url: url}, function (data, textStatus, jqXHR) {
+  var getters = ['curLength'];
+  /* 
+   * Determine whether a method is a getter and doesn't permit chaining.
+   * @param method     (string, optional) the method to run
+   * @param otherArgs  ([], optional) any other arguments for the method
+   * @return true if the method is a getter, false if not 
+   */
+  function isNotChained(method, otherArgs) {
+    if (method === 'option' && (otherArgs.length === 0 ||
+        (otherArgs.length === 1 && typeof otherArgs[0] === 'string'))) {
+      return true;
+    }
+    return $.inArray(method, getters) > -1;
+  }
+
+  /*
+   * Attach the URL shortener functionality to a jQuery selection.
+   * @param options (object) the new settings to use for these instances
+   *                (optional) or (string) the method to run (optional)
+   * @return (jQuery) for chaining further
+   */
+  $.fn.urlshortener = function (options) {
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    if (isNotChained(options, args)) {
+      return plugin['_' + options + 'Plugin'].apply(plugin, [this[0]].concat(args));
+    }
+
+    return this.each(function () {
+      if (typeof options === 'string') {
+        if (!plugin['_' + options + 'Plugin']) {
+          throw 'Unknown method: ' + options;
+        }
+        plugin['_' + options + 'Plugin'].apply(plugin, [this].concat(args));
+      } else {
+        plugin._attachPlugin(this, options || {});
+      }
     });
   };
-}
 
-$.extend(URLShortener.prototype, {
+  var plugin = $.urlshortener = new URLShortener(); // Singleton instance.
 
-});
-
-var plugin = new URLShortener ();
-
-$.fn.shortenURL = function (options) {
-  return this.each(function () {
-    plugin._attachPlugin(this, options || {});
-  });
-};
+})(jQuery);
--- tests/url-shortener-tests.js
+++ tests/url-shortener-tests.js
@@ -1,14 +1,185 @@
-module('Bitly Tests');
+/*jshint es5:true*/
+function setup(options) {
+  this.$input = $('input#input');
+  this.$input.urlshortener(options);
+}
 
-test('Is a valid URL?', function () {
-  ok(validURL('www.google.com'), 'Should be a valid URL');
-  ok(validURL('http://www.google.com'), 'Should be a valid URL');
-  ok(validURL('someurl.cc'), 'Should be a valid URL');
-  ok(validURL('http://someurl.cc'), 'Should be a valid URL');
-  ok(!validURL('http://bit.ly/1234'), 'Bit.ly URLs should not be considered valid');
-  ok(!validURL('bit.ly/1234'), 'Bit.ly URLs should not be considered valid');
-  ok(!validURL('this is not a url'), 'Should not be a valid URL.');
-  ok(!validURL('asdfjlkjalskdfjalsd'), 'Should not be a valid URL.');
-  ok(!validURL('131123123123'), 'Should not be a valid URL.');
+function teardown() {
+  this.$input.urlshortener('destroy');
+}
+
+/**
+ * First and foremost, test for an initialization error. This is in a separate
+ * module so that we can do this without using the setup and teardown methods,
+ * which will error out before we can test for the error.
+ */
+module('Init error', {
+  teardown: function () {
+    $('#input').urlshortener('destroy');
+  }
 });
 
+test('Throw error on initialization.', function () {
+  try {
+    $('input#input').urlshortener();
+    ok(false, 'If you are seeing this, the error was not caught.');
+  } catch (e) {
+    ok(true, 'Throws when no API endpoint has been passed in as an option.');
+
+    ok(
+      !$('input#input').hasClass($.urlshortener.shortenerClass), 
+      'Should not have the plugin class.'
+    );
+
+    ok(
+      typeof $('input#input').data($.urlshortener.propertyName) === 'undefined',
+      'Should not have the plugin\'s instance name as a data attribute.'
+    );
+  }
+});
+
+module('Initialization', {
+  setup: function () {
+    var options = {
+      api: '/bitly/endpoint/'
+    };
+
+    setup.apply(this, [options]);
+  },
+  teardown: function () {
+    teardown.apply(this, arguments);
+  }
+});
+
+test('Options', function () {
+  expect(3);
+
+  deepEqual(this.$input.urlshortener('option'), {
+    api: '/bitly/endpoint/',
+    urlRE: /(https?:\/\/)?((\w+:{0,1}\w*@)?(\S+)\.[a-zA-Z]{2,})(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/,
+    shortenerRE: /bit\.ly/, // Regex for URL shortening service.
+    onSuccess: null
+  }, 'Initial settings');
+
+  this.$input.urlshortener('option', {'api': '/googly/endpoint/'});
+
+  equal(
+    this.$input.urlshortener('option', 'api'),
+    '/googly/endpoint/',
+    "Should update API option."
+  );
+
+  deepEqual(this.$input.urlshortener('option'), {
+    api: '/googly/endpoint/',
+    urlRE: /(https?:\/\/)?((\w+:{0,1}\w*@)?(\S+)\.[a-zA-Z]{2,})(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/,
+    shortenerRE: /bit\.ly/, // Regex for URL shortening service.
+    onSuccess: null
+  }, 'Updated settings');
+});
+
+test('Initialize plugin', function () {
+  expect(2);
+
+  ok(this.$input.hasClass('url-shortener'), 'Input should have class "url-shortener".');
+
+  ok(
+    typeof $('input#input').data($.urlshortener.propertyName) !== 'undefined',
+    'Should not have the plugin\'s instance name as a data attribute.'
+  );
+});
+
+module('URL Shortener Tests');
+
+test('Is the URL valid?', function () {
+  expect(9);
+
+  ok($.urlshortener._validURL('www.google.com'), 'Should be a valid URL');
+  ok($.urlshortener._validURL('http://www.google.com'), 'Should be a valid URL');
+  ok($.urlshortener._validURL('someurl.cc'), 'Should be a valid URL');
+  ok($.urlshortener._validURL('http://someurl.cc'), 'Should be a valid URL');
+  ok(!$.urlshortener._validURL('http://bit.ly/1234'), 'Bit.ly URLs should not be considered valid');
+  ok(!$.urlshortener._validURL('bit.ly/1234'), 'Bit.ly URLs should not be considered valid');
+  ok(!$.urlshortener._validURL('this is not a url'), 'Should not be a valid URL.');
+  ok(!$.urlshortener._validURL('asdfjlkjalskdfjalsd'), 'Should not be a valid URL.');
+  ok(!$.urlshortener._validURL('131123123123'), 'Should not be a valid URL.');
+});
+
+module('Accessing Methods', {
+  setup: function () {
+    var options = {
+      api: '/bitly/endpoint/'
+    };
+
+    setup.apply(this, [options]);
+  },
+  teardown: function () {
+    teardown.apply(this, arguments);
+  }
+});
+
+test('Invoke a plugin method', function () {
+  try {
+    ok(
+      this.$input.urlshortner('invalidMethod'), 
+      "This test should not run because of an invalid method"
+    );
+  } catch (e) {
+    ok(true, "Should catch an error thrown from invoking an invalid method.");
+  }
+
+  ok(this.$input.urlshortener("option"), "Should not throw an error.");
+});
+
+test('Disable and enable plugin', function () {
+  expect(4);
+
+  this.$input.urlshortener("disable");
+
+  equal(this.$input.prop("disabled"), true, "Should be disabled.");
+  ok(this.$input.hasClass('url-shortener-disabled'), "Should have class 'url-shortener-disabled'");
+
+  this.$input.urlshortener("enable");
+
+  equal(this.$input.prop("disabled"), false, "Should be enabled.");
+  ok(!this.$input.hasClass('url-shortener-disabled'), "Should not have class 'url-shortener-disabled'");
+});
+
+test("Destroy plugin", function () {
+  expect(2);
+
+  this.$input.urlshortener("destroy");
+
+  ok(!this.$input.hasClass('url-shortener-disabled'), "Should not have class 'url-shortener-disabled'");
+
+  ok(
+    typeof $('input#input').data($.urlshortener.propertyName) === 'undefined',
+    "Should not have the plugin's instance name as a data attribute."
+  );
+});
+
+module("API", {
+  setup: function () {
+    var options = {
+      api: '/bitly/endpoint/'
+    };
+
+    setup.apply(this, [options]);
+  },
+  teardown: function () {
+    teardown.apply(this, arguments);
+  }
+});
+
+test("API callback", function () {
+  expect(2);
+
+  var spy1 = this.stub(jQuery, 'post').callsArg(2);
+  var callback = this.stub();
+  this.$input.urlshortener('option', {'onSuccess': callback});
+  this.$input.urlshortener('option', {'api': '/bitly/'});
+
+  $.urlshortener.shortenURL(this.$input[0], 'www.mobileroadie.com');
+
+  ok($.post.calledOnce);
+  ok(callback.calledOnce);
+});
